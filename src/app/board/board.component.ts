3a9a8d790ed56a0ab7277e81abbb103a7c309049
Lilit Mazmanyan <lilit.mazmanyan1@gmail.com>
2020-11-18 16:16:24 +0400

    Added a function to check who the winner is

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,36 +1,52 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
-export class BoardComponent implements OnInit {
-  squares: any[];
+export class BoardComponent {
+  squares: string[];
   xIsNext: boolean;
+  winner: string;
 
-  ngOnInit(): void {
-    this.newGame();
-  }
 
-  // tslint:disable-next-line:typedef
   newGame() {
     this.squares = Array(9).fill(null);
+    this.winner = null;
     this.xIsNext = true;
   }
 
-  // tslint:disable-next-line:typedef
   get player() {
     return this.xIsNext ? 'X' : 'O';
   }
 
-  // tslint:disable-next-line:typedef
   makeMove(i: number) {
     if (!this.squares[i]) {
       this.squares.splice(i, 1, this.player);
       this.xIsNext = !this.xIsNext;
     }
+
+    this.winner = this.calculateWinner();
   }
 
-
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (this.squares[a] && this.squares[a] === this.squares[b] && this.squares[a] === this.squares[c]) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
 }
