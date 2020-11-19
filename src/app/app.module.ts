a22f1577d0efbff460df1aaeec9967651739c08f
Lilit Mazmanyan <lilit.mazmanyan1@gmail.com>
2020-11-19 17:37:48 +0400

    Removed square component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BoardComponent } from './board/board.component';
-import { SquareComponent } from './square/square.component';
-import { IntroductionComponent } from './introduction/introduction.component';
+import {AppComponent} from './app.component';
+import {BoardComponent} from './board/board.component';
+import {IntroductionComponent} from './introduction/introduction.component';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
-    SquareComponent,
     IntroductionComponent
   ],
   imports: [
@@ -21,4 +19,6 @@ import {FormsModule} from '@angular/forms';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
